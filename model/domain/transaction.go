4540688d90e8c7fb9a1497d4ef71dc5f741f0ce6
FarizFahmi <fariz.faturachmad@pcsindonesia.co.id>
2024-07-09 20:29:23 +0700

    chore: update foldering

--- model/domain/transaction.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-// TODO: All result from database should be here
-type Transaction struct {
-	Id     string `json:"id" bson:"_id"`
-	UserId string `json:"user_id" bson:"user_id"`
-	TrxId  string `json:"trx_id" bson:"trx_id"`
-	Amount int    `json:"amount" bson:"amount"`
-}
--- model/web/transaction.go
+++
@@ -1,8 +0,0 @@
-package web
-
-// TODO: All request and response should be here
-type Transaction struct {
-	UserId string `json:"user_id"`
-	TrxId  string `json:"trx_id"`
-	Amount int    `json:"amount"`
-}
